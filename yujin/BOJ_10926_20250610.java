b3b65da57b17ff4f205e792082a60f73a45a25bb
breadbirds <uuujin0415@gmail.com>
2025-06-10 23:01:28 +0900

    yujin: 2025-06-10

---
+++ yujin/BOJ_10926_20250610.java
@@ -0,0 +1,13 @@
+package Algo;
+
+import java.util.Scanner;
+
+public class BOJ_10926_20250610 {
+	public static void main(String[] args) {
+		String id;
+		Scanner sc = new Scanner(System.in);
+		id = sc.next();
+
+		System.out.println(id + "??!");
+	}
+}
