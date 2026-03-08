2430466f7c16ed86761c57ff1d2e3f54ab2d0946
breadbirds <uuujin0415@gmail.com>
2026-03-08 19:31:24 +0000

    yujin: 2026-03-08 오늘도 해냈따

---
+++ yujin/백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+import java.util.HashSet;import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		HashSet<String> s = new HashSet<>();
+		for(int i = 0; i < N; i++) {
+			s.add(br.readLine());
+		}
+		
+		int cnt = 0;
+		
+		for(int i = 0; i < M; i++) {
+			if(s.contains(br.readLine())) cnt++;
+		}
+		
+		System.out.println(cnt);
+		
+	}
+
+}
